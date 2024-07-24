3c4cd6d32add3241e6e4ad11120929437119cce1
Nameless1004 <jjho1029@gmail.com>
2024-07-24 11:34:03 +0900

    [Level 2] 2. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,10 +7,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        Queue<Integer> q = new LinkedList<Integer>();
+        Calculator calc = new Calculator();
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -20,50 +18,30 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char op = sc.nextLine().charAt(0);
 
-            int result = 0;
+            try {
+                int result = calc.calculate(firstNum, secondNum, op);
+                System.out.println("결과: " + result);
 
-            switch (op) {
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-                case '*':
-                    result = firstNum * secondNum;
-                    break;
-                case '/':
-                    if (secondNum == 0) {
-                        System.out.println("분모는 0이 될 수 없습니다.");
-                        return;
-                    }
-                    result = firstNum / secondNum;
-                    break;
-                default:
-                    System.out.println("+ - * / 중 하나를 입력해주세요.");
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String input = sc.nextLine();
+                if (input.equals("remove")) calc.getQueue().poll();
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                input = sc.nextLine();
+                if (input.equals("inquiry")) {
+                    // 큐 순회하면서 출력
+                    calc.getQueue().forEach(System.out::println);
+                }
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                input = sc.nextLine();
+
+                if (input.equals("exit")) {
                     return;
+                }
             }
-
-            System.out.println("결과: " + result);
-            // 가장 먼저 저장된 결과를 빼기 위해 FIFO queue 사용
-            q.add(result);
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String input = sc.nextLine();
-            if(input.equals("remove")) q.poll();
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            input = sc.nextLine();
-            if(input.equals("inquiry")) {
-                // 큐 순회하면서 출력
-                q.forEach(System.out::println);
-            }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            input = sc.nextLine();
-
-            if (input.equals("exit")) {
-                return;
+            catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
