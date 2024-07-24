1a74d0f1ee4b393a29c5f429e647e3e5c0f542cd
Nameless1004 <jjho1029@gmail.com>
2024-07-24 10:58:29 +0900

    [Level 1] 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,5 +11,7 @@ public class App {
         int firstNum = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNum = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char op = sc.next().charAt(0);
     }
 }
