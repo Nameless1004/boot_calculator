3d602fe622e5558554006a484bce95ab7875582a
Nameless1004 <jjho1029@gmail.com>
2024-07-24 11:01:50 +0900

    [Level 1] 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,37 +7,45 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNum = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNum = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char op = sc.next().charAt(0);
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNum = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNum = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char op = sc.next().charAt(0);
 
-        int result = 0;
+            int result = 0;
 
-        switch (op){
-            case '+':
-                result = firstNum + secondNum;
-                break;
-            case '-':
-                result = firstNum - secondNum;
-                break;
-            case '*':
-                result = firstNum * secondNum;
-                break;
-            case '/':
-                if(secondNum == 0) {
-                    System.out.println("분모는 0이 될 수 없습니다.");
+            switch (op) {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+                case '/':
+                    if (secondNum == 0) {
+                        System.out.println("분모는 0이 될 수 없습니다.");
+                        return;
+                    }
+                    result = firstNum / secondNum;
+                    break;
+                default:
+                    System.out.println("+ - * / 중 하나를 입력해주세요.");
                     return;
-                }
-                result = firstNum / secondNum;
-                break;
-            default:
-                System.out.println("+ - * / 중 하나를 입력해주세요.");
-                return;
-        }
+            }
 
-        System.out.println("결과: " + result);
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            var input = sc.nextLine();
+            if (input.equals("exit")) {
+                return;
+            }
+        }
     }
 }
