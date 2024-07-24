48231c1941f95d3f2a79208df951c991302d5283
Nameless1004 <jjho1029@gmail.com>
2024-07-24 13:51:31 +0900

    주석추가

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -40,11 +40,17 @@ public class ArithmeticCalculator extends Calculator{
     public <T extends Number> T calculate(T firstNum, T secondNum, char opCode) throws Exception{
         // 인터페이스를 활용한 다형성으로 구현하였습니다.
         // 나누기의 경우 따로 예외를 던지게 하였습니다.
+
+        /*
+            자바에서 제네릭에는 원시타입이 들어가지 않고, Integer도 받아야하고 Double도 받아야하므로
+            상위 클래스인 Number로 타입을 지정해주었습니다.
+         */
         result = operator.get(getOperatorType(opCode)).operate(firstNum, secondNum);
         calcResults.add(result);
         return (T)result;
     }
 
+    // 람다식으로 조건을 받고 stream()의 filter를 사용해 필터링을 해주었습니다.
     public void inquiryFiltering(Predicate<Number> predicate){
         calcResults.stream().filter(predicate).forEach(System.out::println);
     }
--- src/main/java/calculator/Operator/ModOperator.java
+++ src/main/java/calculator/Operator/ModOperator.java
@@ -4,10 +4,12 @@ public class ModOperator implements Operatable{
     @Override
     public <T extends Number> T operate(T a, T b) {
         if(a instanceof Double || a instanceof Float || b instanceof Double || b instanceof Float){
+            if(b.doubleValue() == 0) throw new ArithmeticException("분모는 0이 될 수 없습니다.");
             Double d = a.doubleValue() % b.doubleValue();
             return (T)d;
         }
         else {
+            if(b.intValue() == 0) throw new ArithmeticException("분모는 0이 될 수 없습니다.");
             Integer i = a.intValue() % b.intValue();
             return (T)i;
         }
