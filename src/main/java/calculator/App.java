8b503744128ec4f6518de215634639504736d451
Nameless1004 <jjho1029@gmail.com>
2024-07-24 11:12:12 +0900

    [Level 1] 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -7,13 +8,15 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] results = new int[3];
+        int index = 0;
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt();
+            int firstNum = Integer.parseInt(sc.nextLine());
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = sc.nextInt();
+            int secondNum =  Integer.parseInt(sc.nextLine());
             System.out.print("사칙연산 기호를 입력하세요: ");
-            char op = sc.next().charAt(0);
+            char op = sc.nextLine().charAt(0);
 
             int result = 0;
 
@@ -40,9 +43,22 @@ public class App {
             }
 
             System.out.println("결과: " + result);
-
+            if(index == results.length){
+                for(int i = 0; i < results.length - 1; i++){
+                    // swap
+                    int tmp = results[i];
+                    results[i] = results[i+1];
+                    results[i+1] = tmp;
+                }
+                results[results.length - 1] = result;
+            }
+            else{
+                results[index++] = result;
+            }
+            System.out.println(Arrays.toString(results));
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            var input = sc.nextLine();
+            String input = sc.nextLine();
+
             if (input.equals("exit")) {
                 return;
             }
