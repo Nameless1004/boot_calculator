5f43b56717bd6269acd595e988bfc7131be7249f
Nameless1004 <jjho1029@gmail.com>
2024-07-24 12:53:35 +0900

    [Level 3] 1. 현재 사칙연산 계산기는 +, -, *, /, % 이렇게 총 5가지 연산 타입으로 구성되어있습니다.

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -1,22 +1,46 @@
 package calculator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArithmeticCalculator extends Calculator{
-    private Operatable operator;
+    public ArithmeticCalculator(){
+        operator = new HashMap<>();
+        opCodeMap = new HashMap<>();
+        initOperator();
+    }
 
-    public double calculate(int firstNum, int secondNum, char op) throws Exception{
-        operator = switch (op) {
-            case '+'-> new AddOperator();
-            case '-' -> new SubtractOperator();
-            case '*' -> new MultiplyOperator();
-            case '/' -> new DivideOperator();
-            case '%' -> new ModeOperator();
-            default -> throw new Exception("+ - * / 중 하나를 입력해주세요.");
-        };
+    private void initOperator() {
+        opCodeMap.put('+', OperatorType.ADD);
+        opCodeMap.put('-', OperatorType.SUB);
+        opCodeMap.put('*', OperatorType.MUL);
+        opCodeMap.put('/', OperatorType.DIV);
+        opCodeMap.put('%', OperatorType.MOD);
 
+        // 오퍼레이터타입을 키, 오퍼레이터 객체를 값으로 하는 맵 생성
+        // 이후 사용 시 map에서 해당 키에 해당하는 오퍼레이터를 가져와서 연산
+        operator.put(OperatorType.ADD, new AddOperator());
+        operator.put(OperatorType.SUB, new SubtractOperator());
+        operator.put(OperatorType.MUL, new MultiplyOperator());
+        operator.put(OperatorType.DIV, new DivideOperator());
+        operator.put(OperatorType.MOD, new ModOperator());
+    }
+
+    private Map<Character, OperatorType> opCodeMap;
+    private Map<OperatorType, Operatable> operator;
+
+    public OperatorType getOperatorType(char opCode) throws Exception{
+        var t = OperatorType.valueOf(String.valueOf(opCode));
+        if(!opCodeMap.containsKey(opCode)) throw new Exception("+ - * / % 중에 하나를 입력하세요.");
+        return opCodeMap.get(opCode);
+    }
+
+    public double calculate(int firstNum, int secondNum, char opCode) throws Exception{
         // 인터페이스를 활용한 다형성으로 구현하였습니다.
         // 나누기의 경우 따로 예외를 던지게 하였습니다.
-        result = operator.operate(firstNum,secondNum);
+        result = operator.get(getOperatorType(opCode)).operate(firstNum,secondNum);
         calcResults.add(result);
         return result;
     }
+
 }
